165115059c9e77b2d7a9566dc867cd121c06ce56
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-10-03 16:08:36 -0500

    variable types: string, integer, boolean, char, double

--- Week2_Assignment1_Variables/Program.cs
+++ Week2_Assignment1_Variables/Program.cs
@@ -10,6 +10,18 @@ namespace Week2_Assignment1_Variables
     {
         static void Main(string[] args)
         {
+            string Hello = "Hello World";
+            int Columbus = 1492;
+            bool myPet = false;
+            string Today = "10/03/16";
+            double Amount = 5.45;
+            char letter = 'X';
+            Console.WriteLine(Hello);
+            Console.WriteLine("Columbus sailed the world in " + Columbus);
+            Console.WriteLine("It is " + myPet + " that I have a dog at home.");
+            Console.WriteLine("Today is " + Today);
+            Console.WriteLine("I spent $" + Amount + " on a cheeseburger.");
+            Console.WriteLine(letter + " marks the spot.");
         }
     }
 }
